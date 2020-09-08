3a67197a3bd927042e98f57ba73eb6d9b7de0edf
Marcos Lipic <mjlipic.37@gmail.com>
2020-09-08 18:09:40 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -19,7 +19,22 @@ def consolidate_cart(cart)
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   
   new_consolidated_cart = []
-  binding.pry 
+  counter = 0 
+  while counter < cart.length 
+  new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_consolidated_cart)
+  if new_cart_item
+    new_cart_item[:count] += 1
+  else
+    new_cart_item = {
+      :item => cart[counter][:item],
+      :price => cart[counter][:price],
+      :clearance => cart[counter][:clearance],
+      :count => 1 
+    }
+    new_consolidated_cart << new_cart_item
+  end 
+  counter += 1 
+end 
   return new_consolidated_cart
 end
 
